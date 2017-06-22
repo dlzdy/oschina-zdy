016dbf568e193792666081a7a3d904fe9dc08d64
zhangdy <dlzdy@163.com>
2017-06-22 10:11:25 +0800

    init

---
+++ oschina-zdy/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  oschina-zdy
+//
+//  Created by zhangdy on 17/6/22.
+//  Copyright © 2017年 zhangdy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ oschina-zdy/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  oschina-zdy
+//
+//  Created by zhangdy on 17/6/22.
+//  Copyright © 2017年 zhangdy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+//
+
+@end
+
